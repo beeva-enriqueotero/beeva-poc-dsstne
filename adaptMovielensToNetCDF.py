641baecacb803b5eaf1e8db43164d322e1844347
carlos.gonzalez <carlos.gonzalez@beeva.com>
2016-10-21 13:27:30 +0200

    Added more options

--- adaptMovielensToNetCDF.py
+++ adaptMovielensToNetCDF.py
@@ -4,6 +4,7 @@ Prepares data to go through the generatenetcdf amazon script
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import re
 
 def prepare_100k_dataset(origin_path, dest_path):
     """
@@ -43,6 +44,21 @@ def prepare_20m_dataset(origin_path, dest_path):
         f.write(user_line + '\n')
     f.close()
 
+def recommendations(recs_path):
+    """
+    Analyze recommendations and prepare them to be measured
+    :return:
+    """
+    with open(recs_path) as f:
+        lines = f.readlines()
+        parsed_elements = map(lambda s: re.sub(',[0-9.]+:',',', s.rstrip('\n')).rstrip(',:').split('\t'),lines)
+        recomendations_length = map(lambda s: len(s[1].split(',')),parsed_elements)
+        print "Total recommendations {}".format(len(parsed_elements))
+        incomplete_rec = filter(lambda x: x!=10,recomendations_length)
+        print "Number of incomplete recommendations {} ".format(len(incomplete_rec))
+        return parsed_elements
+
 if __name__ == '__main__':
-    prepare_100k_dataset('u1.base', 'ml100k-u1')
+    #prepare_100k_dataset('u1.base', 'ml100k-u1')
     #prepare_20m_dataset('ratings.csv', 'ml-20m')
+    recommendations('recs')
