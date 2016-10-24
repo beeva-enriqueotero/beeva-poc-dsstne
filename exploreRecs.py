75fcfd8fa0cf600af8c9d782d2a3c03ecc79eb55
carlos.gonzalez <carlos.gonzalez@beeva.com>
2016-10-24 12:22:37 +0200

    formatted for MAP operations

--- exploreRecs.py
+++ exploreRecs.py
@@ -1,6 +1,6 @@
 import re
 import sys
-
+import json
 def recommendations(recs_path):
     """
     Analyze recommendations and prepare them to be measured
@@ -9,14 +9,18 @@ def recommendations(recs_path):
     with open(recs_path) as f:
         lines = f.readlines()
         parsed_elements = map(lambda s: re.sub(',[0-9.]+:',',', s.rstrip('\n')).rstrip(',:').split('\t'),lines)
+        formatted_elements = map(lambda x: {'user':x[0], 'recommendation': x[1].split(',')}, parsed_elements)
         recomendations_length = map(lambda s: len(s[1].split(',')),parsed_elements)
         print "Total recommendations {}".format(len(parsed_elements))
         incomplete_rec = filter(lambda x: x!=10,recomendations_length)
         print "Number of incomplete recommendations {} ".format(len(incomplete_rec))
-        return parsed_elements
+        return formatted_elements
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        recommendations(sys.argv[1])
+        info = recommendations(sys.argv[1])
+        f = open(sys.argv[1] + 'parsed.csv', 'w+')
+        f.write(json.dumps(info))
+        f.close()
     else:
         print 'Usage: \npython exploreRecs.py pathfile'
